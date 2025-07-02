8391fd45ff611a2cfce8d3ac45640ad03b558b33
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-02 17:42:28 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,10 +1,10 @@
-& "$PSScriptRoot\..\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+& "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFilesPath "$PSScriptRoot\..\game\<mod folder>" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -OutputFilePrefix "SMOD_4.0" `
+  -ModName "SMOD 4.0" `
+  -ModFilesPath "$PSScriptRoot\..\game\smod" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/poc-hl2-legacy-registry/releases/download/smod/smod40_full-Original.zip" `
+  -ModBaseFilesUrlHash "fd70970a1548d424a4a1fb1fc77f08f28734f59d11e3a6825dd9ebaed0c4d59c" `
+  -ModReadmePath "readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
